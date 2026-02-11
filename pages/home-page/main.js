ac0e640fda7729e5f54956ae8fbac37768afeec0
rawan <20241700319@cis.asu.edu.eg>
2026-02-11 22:08:34 +0200

    main changes

---
+++ pages/home-page/main.js
@@ -0,0 +1,32 @@
+let menuIcon = document.getElementById("menuToggle");
+let menu = document.querySelector(".mobile-menu");
+let close_icon = document.getElementById("menutog");
+
+menuIcon.addEventListener("click", function(){
+       menu.style.display = "flex";
+
+});
+
+
+
+const link_pages = document.querySelectorAll(".home , .events , .team , .comm");
+
+link_pages.forEach(link_pages =>{
+    link_pages.addEventListener("click",()=>{
+        let class_name = link_pages.className;
+        let link = "";
+        if(class_name === "home") link="../home-page/main.html";
+        else if(class_name === "events") link = "/pages/Events-page/events.html";
+        else if(class_name === "team") link = "../Team-page/team.html";
+        else if(class_name === "comm") link = "../commitees-page/comm.html";
+
+
+        window.location.href = link;
+        menu.style.display = "none";
+    });
+});
+
+
+close_icon.addEventListener("click",()=>{
+    menu.style.display = "none";
+});
