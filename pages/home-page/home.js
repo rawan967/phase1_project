1632758375e506e6631944d7eab0b54916341af8
rawan <20241700319@cis.asu.edu.eg>
2026-02-13 17:02:38 +0200

    some change

--- pages/home-page/home.js
+++ pages/home-page/home.js
@@ -1,9 +1,1 @@
-let track = document.getElementById("track");
-let slideIndex = 0;
 
-setInterval(() => {
-  slideIndex++;
-  if (slideIndex > 4) slideIndex = 0;
-
-  track.style.transform = `translateX(-${slideIndex * 1000}px)`;
-}, 2500);
